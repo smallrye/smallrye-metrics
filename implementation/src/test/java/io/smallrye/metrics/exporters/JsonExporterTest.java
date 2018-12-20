ea6107ab0c48ff259567b063a206ca6471e4a937
Peter Steiner <peter.steiner@acm.org>
2018-12-20 19:07:52 +0100

    Fix the exporters for type METERED
    https://github.com/smallrye/smallrye-metrics/issues/51
    Signed-off-by: Peter Steiner <peter.steiner@acm.org>

---
+++ implementation/src/test/java/io/smallrye/metrics/exporters/JsonExporterTest.java
@@ -0,0 +1,40 @@
+package io.smallrye.metrics.exporters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.microprofile.metrics.Meter;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.junit.Test;
+
+import io.smallrye.metrics.MetricRegistries;
+import io.smallrye.metrics.app.MeterImpl;
+
+public class JsonExporterTest {
+
+    @Test
+    public void exportOneMetric_metered() {
+
+        // maybe this should be System.getProperty("line.separator") ?
+        // but JsonExporter just uses "\n"
+        final String LINE_SEPARATOR = "\n";
+
+        JsonExporter exporter = new JsonExporter();
+        MetricRegistry applicationRegistry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
+
+        // the export should behave identical for any class derived from Meter
+        Meter[] meters = { new MeterImpl(), new SomeMeter() };
+        int idx = 0;
+        for (Meter m : meters) {
+            String name = "json_meter_" + idx++;
+            applicationRegistry.register(name, m);
+            StringBuffer out = exporter.exportOneMetric(MetricRegistry.Type.APPLICATION, name);
+            assertNotNull(out);
+            List<String> lines = Arrays.asList(out.toString().split(LINE_SEPARATOR));
+            assertEquals(1, lines.stream().filter(line -> line.contains("\"" + name + "\"")).count());
+            assertEquals(1, lines.stream().filter(line -> line.contains("\"count\": 0")).count());
+        }
+    }}
