5de39a5584ba9fcbebca7e63af0c600f824a5c48
Jan Martiska <jmartisk@redhat.com>
2020-09-25 08:04:05 +0200

    Deprecate FunctionGaugeSupport

--- api/src/main/java/io/smallrye/metrics/api/FunctionGaugeSupport.java
+++ api/src/main/java/io/smallrye/metrics/api/FunctionGaugeSupport.java
@@ -11,6 +11,7 @@ import io.smallrye.common.annotation.Experimental;
  * Temporary tool to support function gauges in 2.4.x even though they become part of the official API in 3.0.
  */
 @Experimental("Temporarily backported API from Metrics 3.0 to support it in 2.4.x")
+@Deprecated
 public interface FunctionGaugeSupport {
 
     <T extends Number> Gauge<T> gauge(String name, Supplier<T> supplier, Tag... tags);
