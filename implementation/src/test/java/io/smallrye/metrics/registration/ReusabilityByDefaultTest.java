1bc34cea6bb59090326fc91f3fbf3b615ade5f4d
Jan Martiška <jmartisk@redhat.com>
2021-02-04 12:01:44 +0100

    Restore ReusabilityByDefaultTest

--- implementation/src/test/java/io/smallrye/metrics/registration/ReusabilityByDefaultTest.java
+++ implementation/src/test/java/io/smallrye/metrics/registration/ReusabilityByDefaultTest.java
@@ -21,31 +21,43 @@ import static org.junit.Assert.assertEquals;
 
 import java.time.Duration;
 
-import org.eclipse.microprofile.metrics.Gauge;
 import org.eclipse.microprofile.metrics.Metadata;
 import org.eclipse.microprofile.metrics.MetricFilter;
-import org.eclipse.microprofile.metrics.MetricID;
 import org.eclipse.microprofile.metrics.MetricRegistry;
-import org.eclipse.microprofile.metrics.MetricType;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.junit.After;
-import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.smallrye.metrics.MetricRegistries;
 
 /**
- * Verify that programmatically created metrics can be reused by default.
+ * Verify that programmatically created metrics can be reused.
  */
-@Ignore // FIXME
 public class ReusabilityByDefaultTest {
 
     private final MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
 
     @After
-    public void cleanup() {
+    public void removeMetrics() {
         registry.removeMatching(MetricFilter.ALL);
+    }
+
+    static MeterRegistry rootRegistry;
+
+    @BeforeClass
+    public static void addRegistries() {
+        rootRegistry = new SimpleMeterRegistry();
+        Metrics.addRegistry(rootRegistry);
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        Metrics.removeRegistry(rootRegistry);
     }
 
     @Test
@@ -59,7 +71,7 @@ public class ReusabilityByDefaultTest {
     public void testHistogram() {
         registry.histogram("myhistogram").update(5);
         registry.histogram("myhistogram").update(3);
-        assertEquals(3, registry.histogram("myhistogram").getSnapshot().getMin());
+        assertEquals(2, registry.histogram("myhistogram").getCount());
     }
 
     @Test
@@ -75,7 +87,7 @@ public class ReusabilityByDefaultTest {
         Metadata metadata = Metadata.builder().withName("mytimer").build();
         registry.timer(metadata).update(Duration.ofNanos(5));
         registry.timer(metadata).update(Duration.ofNanos(7));
-        assertEquals(7, registry.timer("mytimer").getSnapshot().getMax());
+        assertEquals(2, registry.timer("mytimer").getCount());
     }
 
     @Test
@@ -97,38 +109,6 @@ public class ReusabilityByDefaultTest {
             registry.concurrentGauge(metadata).dec();
             registry.concurrentGauge(metadata).dec();
         }
-    }
-
-    /**
-     * Gauges should not be reusable.
-     * However, verify that after removing a gauge, it is possible to register a new one.
-     */
-    @Test
-    public void gaugeReregistration() {
-        Metadata metadata = Metadata
-                .builder()
-                .withType(MetricType.GAUGE)
-                .withName("mygauge")
-                .build();
-        registry.register(metadata, (Gauge<Long>) () -> 42L);
-        assertEquals(42L, registry.getGauges().get(new MetricID("mygauge")).getValue());
-        try {
-            registry.register(metadata, (Gauge<Long>) () -> 43L);
-            Assert.fail("Should not be able to re-register a gauge");
-        } catch (IllegalArgumentException ex) {
-            // OK
-        }
-
-        // after removing the gauge, I should be able to register it again with different metadata and implementation object
-        registry.remove("mygauge");
-
-        Metadata metadata2 = Metadata.builder()
-                .withType(MetricType.GAUGE)
-                .withName("mygauge")
-                .withDescription("My awesome gauge")
-                .build();
-        registry.register(metadata2, (Gauge<Long>) () -> 45L);
-        assertEquals(45L, registry.getGauges().get(new MetricID("mygauge")).getValue());
     }
 
 }
