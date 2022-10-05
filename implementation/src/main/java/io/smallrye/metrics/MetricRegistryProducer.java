17afb23d1486036d771044a9d88900fbe42fbb5b
David Chan <chdavid@ca.ibm.com>
2022-10-05 10:55:37 -0400

    Producer to produce with RegistryType  Qualifier + update to MicroProfile Metrics 5.0-RC3

--- implementation/src/main/java/io/smallrye/metrics/MetricRegistryProducer.java
+++ implementation/src/main/java/io/smallrye/metrics/MetricRegistryProducer.java
@@ -7,6 +7,7 @@ import jakarta.enterprise.inject.spi.InjectionPoint;
 
 import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.eclipse.microprofile.metrics.annotation.RegistryScope;
+import org.eclipse.microprofile.metrics.annotation.RegistryType;
 
 @ApplicationScoped
 public class MetricRegistryProducer {
@@ -26,6 +27,24 @@ public class MetricRegistryProducer {
             String annoScope = registryScopeAnnotation.scope();
             return SharedMetricRegistries.getOrCreate(annoScope);
         }
+    }
+
+    @Produces
+    @RegistryType(type = MetricRegistry.Type.APPLICATION)
+    public MetricRegistry getApplicationRegistry() {
+        return SharedMetricRegistries.getOrCreate(MetricRegistry.APPLICATION_SCOPE);
+    }
+
+    @Produces
+    @RegistryType(type = MetricRegistry.Type.BASE)
+    public MetricRegistry getBaseRegistry() {
+        return SharedMetricRegistries.getOrCreate(MetricRegistry.BASE_SCOPE);
+    }
+
+    @Produces
+    @RegistryType(type = MetricRegistry.Type.VENDOR)
+    public MetricRegistry getVendorRegistry() {
+        return SharedMetricRegistries.getOrCreate(MetricRegistry.VENDOR_SCOPE);
     }
 
 }
